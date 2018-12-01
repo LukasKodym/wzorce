f7a3d6078b2bf0fd598c3ca52322e034cc048742
LukasKodym <lukasz.kodym@gmail.com>
2018-12-01 14:19:52 +0100

    singleton

---
+++ src/main/java/pl/sda/singleton/Main.java
@@ -0,0 +1,23 @@
+package pl.sda.singleton;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ClassSingleton.getUniqueInstatnce();
+        ClassSingleton.getUniqueInstatnce();
+        ClassSingleton.getUniqueInstatnce();
+        ClassSingleton.getUniqueInstatnce();
+        ClassSingleton.getUniqueInstatnce();
+        ClassSingleton.getUniqueInstatnce();
+        ClassSingleton.getUniqueInstatnce();
+        ClassSingleton.getUniqueInstatnce();
+        ClassSingleton.getUniqueInstatnce();
+        ClassSingleton.getUniqueInstatnce();
+        ClassSingleton.getUniqueInstatnce();
+        ClassSingleton.getUniqueInstatnce();
+
+
+    }
+
+}
